270dd2924b5d20b7f19c1a66833deb79ee19c81f
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2024-07-12 21:57:19 +0530

    bug fixes

--- scrollingText.js
+++ scrollingText.js
@@ -1,30 +1,25 @@
-
 window.addEventListener("wheel", function (element) {
-    if (element.deltaY > 0) {
-        gsap.to(".box", {
-          transform: "translate(-200%)",
-          duration: 2.8,
-          repeat: -1,
-          ease: "none",
-        });
+  if (element.deltaY > 0) {
+    gsap.to(".box", {
+      transform: "translate(-200%)",
+      duration: 2.8,
+      repeat: -1,
+      ease: "none",
+    });
 
-        gsap.to(".box img", {
-            rotate: 180
-        })
+    gsap.to(".box img", {
+      rotate: 180,
+    });
+  } else {
+    gsap.to(".box", {
+      transform: "translate(0%)",
+      duration: 2.8,
+      repeat: -1,
+      ease: "none",
+    });
 
-
-    } else {
-        gsap.to(".box", {
-          transform: "translate(0%)",
-          duration: 2.8,
-          repeat: -1,
-          ease: "none",
-        });
-
-        gsap.to(".box img", {
-          rotate: 0,
-        });
-    }
-
-})
-
+    gsap.to(".box img", {
+      rotate: 0,
+    });
+  }
+});
